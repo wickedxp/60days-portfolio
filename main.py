33e5ee28f4422e3a0f92f00371fdc274a6ef9a45
Saul DeJesus <sdejesus01@gmail.com>
2023-12-11 15:34:19 -0500

    render the titles, description, source code link, and image

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 #st.set_page_config(layout='wide')
 
@@ -14,5 +15,25 @@ with col2:
     
     st.write(content)
 
+st.divider()
 content2 = """Below you can find some of the apps I have built in Python. Feel free to contact me!"""
 st.write(content2)
+
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=';')
+    
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.markdown(f"""[source code]({row['url']})""")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.markdown(f"""[source code]({row['url']})""")
