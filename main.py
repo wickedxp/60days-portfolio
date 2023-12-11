70a1849b23ffa243916aac33dce82753329c003e
Saul DeJesus <sdejesus01@gmail.com>
2023-12-11 14:32:14 -0500

    Add description on the new columns

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(layout='wide')
+#st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
 
@@ -10,6 +10,9 @@ with col1:
 with col2:
     st.title("Saul DeJesus")
     content = """Hello, my name is Saul DeJesus! I am a wanna-be Python programmer. Started this journey two years ago and started working 
-    on multiple project since then"""
+    on multiple project since then. Love everything about Python programming"""
     
     st.write(content)
+
+content2 = """Below you can find some of the apps I have built in Python. Feel free to contact me!"""
+st.write(content2)
