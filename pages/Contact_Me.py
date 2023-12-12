ae4a82b0683b30967cef2e15c65e1ce890c5feee
Saul DeJesus <sdejesus01@gmail.com>
2023-12-12 11:33:43 -0500

    latest contact me

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.header('Contact me')
 
+with st.form(key='email_forms'):
+    user_email = st.text_input('Your email address')
+    message = st.text_area('Your Message')
+    submit_button = st.form_submit_button('Submit')
+    if button:
+        
